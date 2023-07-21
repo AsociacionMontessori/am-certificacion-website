990581338f23a1bf7a5c29d476d6365b8c99b8eb
Rodrigo Rubio <chavo0022009@gmail.com>
2023-07-20 22:17:08 -0700

    + certificate

---
+++ src/pages/certificate.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+
+let content = card1.content
+
+const certificate = () => {
+  return (
+    <Layout>
+      
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="Certificate" />
+
+export default certificate
