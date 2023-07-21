53c9d04ee302f13d1e71f3db45a9cc2b8c1c1a8a
Rodrigo Rubio <chavo0022009@gmail.com>
2023-07-20 22:46:37 -0700

    * index: darkmode fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <main>
       <section id="home" class="relative flex min-h-screen items-center">
-        <div aria-hidden="true" class="absolute inset-0 z-[1] bg-gradient-to-b from-white/0 via-gray/20 to-gray"></div>
+        <div aria-hidden="true" class="absolute inset-0 z-[1] bg-gradient-to-b from-white/0 via-white/10 to-white/60  dark:bg-gradient-to-b dark:from-white/0 dark:via-gray/20 dark:to-gray"></div>
         <img src={imagen} class="fixed inset-0 h-full w-full object-cover" alt="woman in dark" width="4160" height="6240" />
         <div class="relative z-10 mx-auto max-w-7xl px-6 pb-40 pt-40 lg:px-12 xl:px-6 2xl:px-0">
           <div class="pb-12 media-h:md:pb-32 media-h:lg:pb-12 xl:pb-12">
@@ -21,64 +21,66 @@ const IndexPage = () => (
               </p>
               <div className="mb-5">
                 <button className="btn btn-outline hover:text-white hover:bg-blue bg-gradient-to-r from-blue/50 hover:from-gray hover:bg-opacity-70 rounded-full px-10 mr-4 bg-blue text-white bg-opacity-10">Certifícate</button>
-                <button className="btn btn-outline hover:text-white hover:bg-green bg-gradient-to-r from-green/50 hover:from-gray hover:bg-opacity-70 rounded-full px-10 mr-4 bg-green text-white bg-opacity-10">Conoce el Método</button>
+                <a href="https://kalpilli.com/MetodoMontessori/" target="__blank" className="btn btn-outline hover:text-white hover:bg-green bg-gradient-to-r from-green/50 hover:from-gray hover:bg-opacity-70 rounded-full px-10 mr-4 bg-green text-white bg-opacity-10">
+                  Conoce el Método
+                </a>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <section id="work" class="relative z-10 bg-gray pb-20 pb-72 lg:pt-0">
+      <section id="work" class="relative py-5 z-10 bg-white dark:bg-gray pb-20 pb-72 lg:pt-0">
         <div class="mx-auto max-w-7xl px-6 lg:px-12 xl:px-6 2xl:px-0 pt-10">
           <div data-rellax-speed="-3" data-rellax-xs-speed="0" data-rellax-mobile-speed="0" class="rellax flex flex-wrap items-center gap-6">
-            <h2 class="text-5xl font-bold text-white xl:text-6xl">EDUCACIÓN SIGNIFICATIVA</h2>
+            <h2 class="pt-5 text-5xl text-red font-bold dark:text-white xl:text-6xl">EDUCACIÓN SIGNIFICATIVA</h2>
             <p className="text-2xl">
-              En la educación significativa, se busca que los <strong className="text-red">estudiantes se involucren de manera activa en su propio proceso de aprendizaje</strong>, relacionando los nuevos conceptos con su conocimiento previo y con su entorno. El objetivo es que los estudiantes no solo adquieran información de manera pasiva, sino que la comprendan, le encuentren sentido y la integren en su propia estructura cognitiva.
+              En la educación significativa, se busca que los <strong className="text-blue dark:text-red">estudiantes se involucren de manera activa en su propio proceso de aprendizaje,</strong> relacionando los nuevos conceptos con su conocimiento previo y con su entorno. El objetivo es que los estudiantes no solo adquieran información de manera pasiva, sino que la comprendan, le encuentren sentido y la integren en su propia estructura cognitiva.
             </p>
           </div>
         </div>
         <div className="overflow-hidden mt-5">
           <section className="my-5 block h-12 overflow-hidden relative w-max marquee-left xxl:hidden">
             <div className="flex float-left w-50">
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-blue rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-blue rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
             </div>
           </section>
           <section className="my-5 block h-12 overflow-hidden relative w-max marquee-left xxl:hidden">
             <div className="flex float-left w-50">
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
-              <label class="bg-black hover:text-green/70 rounded-full py-3 px-10 text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">No se puede ser libre, si no se es independiente</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">Lo que la mano hace, la mente lo recuerda</label>
+              <label class="bg-red/20 dark:bg-black hover:font-semibold dark:hover:text-green/70 rounded-full py-3 px-10 dark:text-white/80 mx-3">El niño crea sus propios movimientos y, una vez creados, los perfecciona</label>
             </div>
           </section>
 
