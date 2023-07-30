58007623c0fae295fd89b71fd63c48af5d226975
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2023-07-30 01:47:55 -0600

    * Timelines fixes

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -10,29 +10,44 @@ const green = fullConfig.theme.colors.green
 const blue = fullConfig.theme.colors.blue
 
 const Timeline = () => {
-    const title = "Conoce la Asociación Montessori"
+    const title = "Asociación Montessori de México - AMMAC"
     const items = [
         {
-            title: 'Fundación de AMMAC',
-            content: 'La Asociación Montessori de México A.C. (Ammac) se fundó con el objetivo de promover la pedagogía de María Montessori en México. Con más de 57 años de experiencia, somos pioneros en la implementación y difusión del método Montessori en todo el mundo. Nuestro equipo está formado por profesionales apasionados por la educación y comprometidos con el crecimiento y desarrollo integral de cada niño.',
-            year: '1964'
+            title: 'Se funda AMMAC',
+            content: 'La Asociación Montessori de México A.C. (Ammac) se fundó con el objetivo de promover la pedagogía de María Montessori en México. \n Con más de 57 años de experiencia, somos pioneros en la implementación y difusión del método Montessori en todo el mundo. Nuestro equipo está formado por profesionales apasionados por la educación y comprometidos con el crecimiento y desarrollo integral de cada niño.',
+            year: '1964',
+            color: "blue"
         },
         {
             title: 'Kalpilli Montessori',
             content: 'Kalpilli Montessori abre sus puertas ... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.',
-            year: '1964'
+            year: '1964',
+            color: "red"
         },
         {
             title: 'Random dots?',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.',
-            year: '1964'
+            year: '1964',
+            color: "blue"
         },
         {
             title: 'Absolutely nothing.',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.',
-            year: '1964'
+            year: '1964',
+            color: "red"
         },
     ];
+
+    const getColor = (color) => {
+        switch (color) {
+            case "red":
+                return red;
+            case "blue":
+                return blue;
+            default:
+                return red;
+        }
+    }
 
     const beforeStyles = {
         content: '""',
@@ -47,57 +62,59 @@ const Timeline = () => {
         animation: 'scaleVertical 3s 1s ease both 1',
     };
 
-    const afterStyles = {
-        content: '""',
-        clear: 'both',
-        position: 'absolute',
-        bottom: '0px',
-        left: '-1.05em',
-        width: '40px',
-        height: '40px',
-        borderRadius: '50%',
-        background: `none repeat scroll 0% 0% ${red}`,
-        border: `5px solid ${red}`,
-        boxShadow: '1px 1px 1px rgba(0, 0, 0, 0.1)',
-        animation: 'revealScaleUp 0.75s 2.82s ease both 1',
-    };
+    // const afterStyles = {
+    //     content: '""',
+    //     clear: 'both',
+    //     position: 'absolute',
+    //     bottom: '0px',
+    //     left: '-1.05em',
+    //     width: '40px',
+    //     height: '40px',
+    //     borderRadius: '50%',
+    //     background: `none repeat scroll 0% 0% ${red}`,
+    //     border: `5px solid ${red}`,
+    //     boxShadow: '1px 1px 1px rgba(0, 0, 0, 0.1)',
+    //     animation: 'revealScaleUp 0.75s 2.82s ease both 1',
+    // };
 
     return (
         <>
             <section className="block-content t-block-teal l-block-spacing pl-10 md:ml-40">
                 <div className="l-contained">
-                    <header className="heading-group">
-                        <h2 className="text-blue dark:text-white text-6xl">{title}</h2>
+                    <header className="heading-group pl-10 sm:pl-0">
+                        <h2 className="text-blue dark:text-white text-3xl sm:text-5xl lg:text-6xl font-bold">{title}</h2>
                     </header>
                     <div>
                         <ul className="timeline-list" style={{ position: 'relative' }}>
                             <div>
                                 <div style={{ ...beforeStyles, ...{ background: `repeating-linear-gradient(0deg, ${red} 0%, ${red} 20%, ${blue} 25%, ${blue} 45%, ${red} 50%, ${red} 70%, ${blue} 75%, ${blue} 100%)` } }}></div>
-                                <div style={afterStyles}></div>
+                                {/* <div style={afterStyles}></div> */}
                             </div>
                             {items.map((item) => (
                                 <li key={item.title}>
                                     <div className="content pt-12 px-6">
-                                        <h3 className="text-white text-6xl px-4 py-2 bg-red w-3/5 rounded-3xl bg-opacity-70">{item.title}</h3>
-                                        <span className="lg:hidden">{item.year}</span>
-                                        <p className="text-black dark:text-white text-3xl pt-5 ">{item.content}</p>
+                                        <span className="lg:hidden text-4xl absolute dark:opacity-60 opacity-30 top-0 font-bold text-blue">{item.year}</span>
+                                        <h3 className="text-black dark:text-whit text-2xl sm:text-3xl md:text-4xl px-4 py-2 mt-4  bg-blue w-full md:w-3/5 rounded-3xl bg-opacity-10">
+                                            {item.title}
+                                        </h3>
+                                        <p className="text-black dark:text-white sm:text-2xl md:text-3xl pt-5 ">{item.content}</p>
                                         <div className="hidden lg:block">
-                                            <span className="absolute dark:opacity-60 opacity-30 top-20 text-8xl font-bold mt-20 -ml-60 -rotate-90 text-blue">{item.year}</span>
+                                            <span className={`absolute dark:opacity-60 opacity-30 top-20 text-8xl font-bold mt-20 -ml-60 -rotate-90 text-${item.color}`}>{item.year}</span>
                                         </div>
                                         <div>
                                             <div className='absolute top-20 mt-60 -ml-60'>
                                                 <svg width='50' height='50'>
-                                                    <circle cx='25' cy='25' r='20' stroke={blue} opacity={.15} stroke-width='2' fill='transparent' />
+                                                    <circle cx='25' cy='25' r='20' stroke={getColor(item.color)} opacity={.15} stroke-width='2' fill='transparent' />
                                                 </svg>
                                             </div>
                                             <div className='absolute top-10 mt-20 -ml-60'>
                                                 <svg width='100' height='110' rotate={25}>
-                                                    <rect transform="rotate(-45 100 50)" x='10' y='10' width='30' height='30' stroke={blue} opacity={.15} stroke-width='2' fill='transparent' />
+                                                    <rect transform="rotate(-45 100 50)" x='10' y='10' width='30' height='30' stroke={getColor(item.color)} opacity={.15} stroke-width='2' fill='transparent' />
                                                 </svg>
                                             </div>
                                             <div className='absolute top-20 mt-64 -ml-40'>
                                                 <svg width='100' height='200'>
-                                                    <rect transform="rotate(22 0 50)" x='10' y='10' width='30' height='30' stroke={blue} opacity={.15} stroke-width='2' fill='transparent' />
+                                                    <rect transform="rotate(22 0 50)" x='10' y='10' width='30' height='30' stroke={getColor(item.color)} opacity={.15} stroke-width='2' fill='transparent' />
                                                 </svg>
                                             </div>
                                         </div>
