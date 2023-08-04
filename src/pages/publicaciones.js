f419aa162f3a2d8ad8da4bcf6ecdc99f6aacadd6
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2023-08-04 09:47:53 -0600

    + Publicaciones

--- src/pages/publicaciones.js
+++ src/pages/publicaciones.js
@@ -5,7 +5,7 @@ import Seo from "../components/seo"
 import imagen from '../images/books/books1.jpg'
 import imagen2 from '../images/books/books2.jpg'
 import '../styles/publications.css'
-
+import Button from '../components/buttons/button';
 
 
 const publicaciones = () => {
@@ -25,8 +25,8 @@ const publicaciones = () => {
                             <article class="max-w-sm mx-auto md:max-w-none grid md:grid-cols-2 gap-4 md:gap-8 lg:gap-12 xl:gap-16">
                                 <div class="relative lg:mt-28 w-1/2 block group" href="#0">
                                     {/* <div class="absolute inset-0 bg-gray-800 hidden md:block transform md:translate-y-2 md:translate-x-4 xl:translate-y-4 xl:translate-x-8 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out pointer-events-none" aria-hidden="true"></div> */}
-                                    <div className="sm:hidden ml-28 absolute w-full z-10">
-                                        <h1 className="text-4xl text-white font-bold drop-shadow-4xl ">Conoce nuestras Publicaciones</h1>
+                                    <div className="sm:hidden ml-20 sm:ml-28 absolute w-full z-10">
+                                        <h1 className="text-3xl sm:text-4xl text-white font-bold drop-shadow-4xl ">Conoce nuestras Publicaciones</h1>
                                     </div>
                                     <div>
                                         <div class="w-1/2 book">
@@ -51,13 +51,24 @@ const publicaciones = () => {
                                         </div>
                                     </header>
                                     <p class="text-lg flex-grow">{description}</p>
-                                    <div class="my-3 w-full">
+                                    <div class="mt-5 mb-3 w-full">
                                         <ul class="flex flex-wrap text-xs font-medium -m-1">
                                             <li class="m-1 z-10">
-                                                <a class="inline-flex text-center text-gray-100 py-3 px-6 rounded-full bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" href="#0">Product</a>
+                                                <a class="inline-flex text-center text-gray-100 py-3 px-6 rounded-full bg-red hover:bg-opacity-90 transition duration-150 ease-in-out" href="#0">
+                                                    Comprar Ahora
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="ml-1 w-4 h-4">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                                                    </svg>
+                                                </a>
                                             </li>
                                             <li class="m-1 z-10">
-                                                <a class="inline-flex text-center text-gray-100 py-3 px-6 rounded-full bg-blue-500 hover:bg-blue-600 transition duration-150 ease-in-out" href="#0">Engineering</a>
+                                                <a class="inline-flex text-center text-gray-100 py-3 px-6 rounded-full bg-blue-500 hover:bg-blue-600 transition duration-150 ease-in-out" href="#0">
+                                                    Ver en Amazon
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-4 h-4" viewBox="0 0 512 512">
+                                                        <path d="M48.48,378.73a300.52,300.52,0,0,0,152.89,95.92,262.57,262.57,0,0,0,159.3-17.25,225.52,225.52,0,0,0,66.79-47,6.36,6.36,0,0,0-2-8.53,11.76,11.76,0,0,0-8-.05,401.92,401.92,0,0,1-116.55,39.34,358.13,358.13,0,0,1-127.29-8.83,446.73,446.73,0,0,1-119.1-60.49,5,5,0,0,0-6.06,6.9Z" fill="white"></path><path d="M387.15,388.44a168.11,168.11,0,0,1,48.94-2.23l.67.13a10,10,0,0,1,7.37,12.05A204.71,204.71,0,0,1,429,444.47a2.55,2.55,0,0,0,1.66,3.18,2.51,2.51,0,0,0,2.23-.37A83.31,83.31,0,0,0,464,382.86a12.44,12.44,0,0,0-10.22-13.22A95.75,95.75,0,0,0,384.91,384a2.55,2.55,0,0,0-.57,3.55A2.52,2.52,0,0,0,387.15,388.44Z" fill="white"></path><path d="M304.24,324.92a164,164,0,0,1-28.92,25.3A135.16,135.16,0,0,1,208.63,369a99.49,99.49,0,0,1-57.49-19.85,97.25,97.25,0,0,1-27.36-100.28,112.35,112.35,0,0,1,65.3-69.06,367.67,367.67,0,0,1,104.7-15.55V127A37.82,37.82,0,0,0,261,94.72a59.9,59.9,0,0,0-31.17,4.08,48.89,48.89,0,0,0-27.13,34.67,12,12,0,0,1-12.58,6.72l-50.9-4.5a11.38,11.38,0,0,1-8.38-10.16,103.66,103.66,0,0,1,36.61-63.45A143.86,143.86,0,0,1,257.85,32a146.24,146.24,0,0,1,84.27,27.67,86.82,86.82,0,0,1,30.7,70.22V258.8a84.46,84.46,0,0,0,8,31.28l15.87,23.23a13,13,0,0,1,0,11.23L349.7,364.25a12.5,12.5,0,0,1-12.68-.44A244.84,244.84,0,0,1,304.24,324.92Zm-10.6-116.83a257.68,257.68,0,0,0-44,2.89A63,63,0,0,0,208,242.54a63,63,0,0,0,3.07,54,40.6,40.6,0,0,0,47.11,12.19,78.61,78.61,0,0,0,35.46-55.58V208.09" fill="white">
+                                                        </path>
+                                                    </svg>
+                                                </a>
                                             </li>
                                         </ul>
                                     </div>
@@ -66,7 +77,10 @@ const publicaciones = () => {
                         </div>
                     </section>
                     <section className="bg-white min-h-screen">
-                        <h1>Hola</h1>
+                        <section class="flex flex-col antialiased text-gray-200 pl-10 md:ml-40 bg-white min-h-screen">
+                            <h2 className="text-black">Contenido Blog: Cargando...</h2>
+                        </section>
+
                     </section>
                 </div>
 
