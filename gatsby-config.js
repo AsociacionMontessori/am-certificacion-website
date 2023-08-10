b64989943a03feee508d1b6f90bdd6f471c5a0a8
Mauricio Rubio Haro <mau.r.h54@gmail.com>
2023-08-09 23:00:06 -0600

    wordpress integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,5 +28,19 @@ module.exports = {
         icon: `src/images/lasc.png`,
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-responsive-iframe`],
+      },
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url:
+          process.env.WPGRAPHQL_URL ||
+          `http://am-website-test.local/graphql`,
+      },
+    },
   ],
 }
