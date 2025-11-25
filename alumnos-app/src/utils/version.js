3dea75e128a7f6ff5f47404dc80a164b0b5dbbb3
carlosrm <git@kalpilli.com>
2025-11-25 13:33:17 -0600

    chore: Bump version to 0.3.0

--- alumnos-app/src/utils/version.js
+++ alumnos-app/src/utils/version.js
@@ -1,6 +1,6 @@
 // Versión de la aplicación
-export const VERSION = '0.2.0';
+export const VERSION = '0.3.0';
 export const VERSION_INFO = {
-  version: '0.2.0',
+  version: '0.3.0',
   buildDate: new Date().toISOString()
 };
